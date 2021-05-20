55912ca6d3351f378aebdf8fa337c5c24a8394a9
Zach <zach.m.yu@gmail.com>
2021-05-20 14:02:11 -0700

    W15D4 Completed Phase 3

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
+const { ValidationError } = require('sequelize');
 const isProduction = environment === 'production';
 const routes = require('./routes');
 
@@ -29,5 +30,37 @@ app.use(
   })
 );
 app.use(routes);
+
+// app.use((req, res, next) => {
+//   res.status(404).render('/404');
+// });
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
