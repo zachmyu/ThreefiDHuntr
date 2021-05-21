bd2ed9900ae8326bca604723b836d31b07aab22a
Zach <zach.m.yu@gmail.com>
2021-05-21 15:25:04 -0700

    Completed sign up route

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const bcrypt = require('bcryptjs');
+const { Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,22 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
+
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { username, fullName, email, about, password } = req.body;
+    const user = await User.signup({ username, fullName, email, about, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
 
 module.exports = router;
