46c34faa955ca2c00885d13eeeac1d6253b66e2b
Zach <zach.m.yu@gmail.com>
2021-05-21 12:29:08 -0700

    Completed Auth 1 Phase 3

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
@@ -50,7 +52,45 @@ module.exports = (sequelize, DataTypes) => {
           attributes: {},
         },
       },
+    },
+  );
+  User.prototype.toSafeObject = function () { // remember, this cannot be an arrow function
+    const { id, username, email } = this; // context will be the User instance
+    return { id, username, email };
+  };
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+  User.getCurrentUserById = async function (id) {
+    return await User.scope('currentUser').findByPk(id);
+  };
+  User.login = async function ({ credential, password }) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
     });
+
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+  User.signup = async function ({ username, fullName, email, about, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      fullName,
+      email,
+      about,
+      hashedPassword,
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
+
   User.associate = function (models) {
     // associations can be defined here
   };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
 
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
