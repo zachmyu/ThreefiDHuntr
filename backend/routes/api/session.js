f7ffb7956ab999c6c7376af4fa798f702a804f90
Zach <zach.m.yu@gmail.com>
2021-05-21 15:54:06 -0700

    Get for user routes from session completed

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
-
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
 
 router.post(
   '/',
