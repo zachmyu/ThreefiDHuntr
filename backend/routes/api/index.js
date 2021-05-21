4eff48d758d60f3e36dd036047f8d59f78724d6a
Zach <zach.m.yu@gmail.com>
2021-05-21 14:06:10 -0700

    Completed user login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 module.exports = router;
