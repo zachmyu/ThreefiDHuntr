d660b4b119257a1cffdb45a6430264b95b49be3d
Zach <zach.m.yu@gmail.com>
2021-05-24 14:14:20 -0700

    Completed logout function

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -37,6 +37,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 export const signup = (user) => async (dispatch) => {
   const { username, fullName, email, about, password } = user;
   const response = await csrfFetch('/api/users', {
