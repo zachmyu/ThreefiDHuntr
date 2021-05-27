ca1142c41c6d503c26382821c38bd9580f86c32d
Zach <zach.m.yu@gmail.com>
2021-05-27 14:31:24 -0700

    Changed Review form into modal, needs troubleshooting

---
+++ frontend/src/components/ReviewFormModal/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import ReviewForm from './ReviewForm';
+
+function ReviewFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Add Review</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <ReviewForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default ReviewFormModal;
